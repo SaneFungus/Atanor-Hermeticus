9bc4a61dae941e93274b7571de3155b043adf0fd
SaneFungus <63272836+SaneFungus@users.noreply.github.com>
2025-05-08 23:44:53 +0200

    Update category-definitions.js

---
+++ js/data/category-definitions.js
@@ -0,0 +1,10 @@
+window.AtanorData = window.AtanorData || {};
+
+// Definicje kategorii funkcjonalnych
+AtanorData.categories = {
+    fixatio: "Utrwalanie i stabilizacja poznawcza",
+    multiplicatio: "Zwielokrotnianie i wzmacnianie punktów dźwigni poznawczej",
+    fermentatio: "Organiczny rozwój i transformacja koncepcji",
+    coniunctio: "Łączenie przeciwieństw i integracja dialektyczna",
+    meta: "Meta-epistemiczne refleksje i transformacje"
+};
